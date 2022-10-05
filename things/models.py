f3bd979683742392cc6127f7759388e1e54b8409
Hui Shing Chong <hui.chong@kcl.ac.uk>
2022-10-05 23:00:04 +0100

    Add more constraints on my model Thing attributes

--- things/models.py
+++ things/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, blank=False, unique=True)
+    name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(unique=True)
+    quantity = models.PositiveIntegerField(unique=True, validators = [MaxValueValidator(100)])
