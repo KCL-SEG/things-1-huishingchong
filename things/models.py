d6b718f27ccab9c6435aa0c15df489f3adcc4256
Hui Shing Chong <hui.chong@kcl.ac.uk>
2022-10-10 11:26:03 +0100

    Add a new directory to implement Database seeder and unseeder

--- things/models.py
+++ things/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(
-        unique=True, 
+    quantity = models.IntegerField(
+        unique=False, 
         validators = [
-        MaxValueValidator(101, "Quantity value cannot be greater than 100.")
+            MinValueValidator(0, "Quantity value must be greater than 0."),
+            MaxValueValidator(100, "Quantity value cannot be greater than 100.")
         ]
     )
